d06d6b47c9ca60169d2585242d86ffb78203ff09
tm.shih <tm.shih@advantech.com.tw>
2020-07-09 13:14:57 +0800

    Support the KeyEvent navigation.

--- app/src/main/java/com/tms/fragmentpager/view/SwipeableViewPager.java
+++ app/src/main/java/com/tms/fragmentpager/view/SwipeableViewPager.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
 
 import com.tms.fragmentpager.R;
 
@@ -35,6 +38,45 @@ public class SwipeableViewPager extends ViewPager {
     /** Set to enable or disable swipe. **/
     public void setSwipeable(boolean flag) {
         isSwipeable = flag;
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (isSwipeable) {
+            return super.dispatchKeyEvent(event);
+        }
+        if (event.getKeyCode() == KeyEvent.KEYCODE_DPAD_LEFT) {
+            View focusView = findFocus();
+            if (focusView instanceof EditText) {
+                EditText editText = (EditText) focusView;
+                if (!editText.isEnabled()) {
+                    return false;
+                }
+                if (editText.getSelectionStart() == editText.getSelectionEnd()
+                        && editText.getSelectionStart() <= 0) {
+                    return false;
+                }
+                return super.dispatchKeyEvent(event);
+            } else {
+                return false;
+            }
+        } else if (event.getKeyCode() == KeyEvent.KEYCODE_DPAD_RIGHT) {
+            View focusView = findFocus();
+            if (focusView instanceof EditText) {
+                EditText editText = (EditText) focusView;
+                if (!editText.isEnabled()) {
+                    return false;
+                }
+                if (editText.getSelectionStart() == editText.getSelectionEnd()
+                        && editText.getSelectionStart() >= editText.getText().length()) {
+                    return false;
+                }
+                return super.dispatchKeyEvent(event);
+            } else {
+                return false;
+            }
+        }
+        return super.dispatchKeyEvent(event);
     }
 
     @Override
